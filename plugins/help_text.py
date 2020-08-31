9473da0a8807a129addf2e0efc2636e901a7e03d
DRAGON-NOOB <69449962+DRAGON-NOOB@users.noreply.github.com>
2020-08-31 11:08:13 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -50,7 +50,7 @@ async def help_user(bot, update):
     TRChatBase(update.from_user.id, update.text, "/help")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=translation.HELP_USER,
+        text=Translation.HELP_USER,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")],
                                            [InlineKeyboardButton(text="🔙 BACK", callback_data="get_start"), InlineKeyboardButton(text="💢 ABOUT", callback_data="get_about"), InlineKeyboardButton(text="✖️ CLOSE", callback_data="close")]]),
         parse_mode="html",
@@ -65,7 +65,7 @@ async def send_start(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text=translation.START_TEXT,
+        text=Translation.START_TEXT,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ CHANNEL ⭕️", url="https://t.me/TroJanzHEX"), InlineKeyboardButton(text="⭕️ SUPPORT ⭕️", url="https://t.me/contactHEXbot")],
                                                     [InlineKeyboardButton(text="🆘 HELP 🆘", callback_data="get_help"), InlineKeyboardButton(text="♐️ SHARE ♐️", url="tg://msg?text=Hai%20Friend%20%E2%9D%A4%EF%B8%8F%2C%0AToday%20i%20just%20found%20out%20an%20intresting%20and%20Powerful%20%2A%2ARename%20Bot%2A%2A%20for%20Free%F0%9F%A5%B0.%20%0A%2A%2ABot%20Link%20%3A%2A%2A%20%40TroJanzRenamer%20%F0%9F%94%A5")]]),
         parse_mode="html",
@@ -93,7 +93,7 @@ async def about(bot, update):
     TRChatBase(update.from_user.id, update.text, "/about")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=translation.ABOUT_TEXT,
+        text=Translation.ABOUT_TEXT,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔙 BACK 🔙", callback_data="get_help"), InlineKeyboardButton(text="✖️ CLOSE ✖️", callback_data="close")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
